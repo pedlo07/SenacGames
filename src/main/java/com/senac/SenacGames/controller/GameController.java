924c3e246a9ef27664f265bab327d2b0423fc378
pedlo07 <pedroalmeidaveber@gmail.com>
2023-08-23 20:36:44 -0300

    Update de código

--- src/main/java/com/senac/SenacGames/controller/GameController.java
+++ src/main/java/com/senac/SenacGames/controller/GameController.java
@@ -3,11 +3,11 @@ package com.senac.SenacGames.controller;
 import com.senac.SenacGames.entities.Game;
 import com.senac.SenacGames.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/games")
@@ -21,6 +21,31 @@ public class GameController {
     @GetMapping
     public List<Game> getTodosOsJogos(){
         return gameRepository.findAll();
+
+    }
+@PostMapping
+    public Game createGame(@RequestBody Game game){return gameRepository.save(game);}
+    @PutMapping("/{id}")
+    public ResponseEntity<Game> updateGame(@PathVariable Long id, @RequestBody Game updatedGame) {
+        Optional<Game> optionalGame = gameRepository.findById(id);
+
+        if (!optionalGame.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Game existingGame = optionalGame.get();
+        existingGame.setTitle(updatedGame.getTitle());
+        existingGame.setGenre(updatedGame.getGenre());
+
+        Game updated = gameRepository.save(existingGame);
+        return ResponseEntity.ok(updated);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteGame(@PathVariable Long id) {
+        gameRepository.deleteById(id);
     }
 
 }
+
